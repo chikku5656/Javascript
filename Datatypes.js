48a03b2292c8346350114d8e6d85b96a80ca69ad
chikku5656 <157700859+chikku5656@users.noreply.github.com>
2024-04-22 10:09:39 +0000

    Datatypesconversion

---
+++ Datatypes.js
@@ -0,0 +1,11 @@
+let Nname = "ujjwal"
+let age = "15"
+
+let a =5
+let b =5 
+console.table ([Nname, age])
+function Name (a , b){
+    console.log("a + b , a + b")
+    return a + b
+}
+Name(4 ,5);
