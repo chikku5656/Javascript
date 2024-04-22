87db5c6fcc9e0171d90c95b75f80e7bc129a8ff4
chikku5656 <157700859+chikku5656@users.noreply.github.com>
2024-04-22 08:53:08 +0000

    datatypes

---
+++ Variables.js
@@ -0,0 +1,12 @@
+ const accountId = 879562
+  var accountEmail = "abc@gmail.com"
+  let accountPassword = "*5465*+545"
+ accountCity = "baddi"
+
+//   accountEmail = "abc@gmail.com"
+//   accountPassword = "*5465*+5"
+//   accountCity ="SOLan"
+// //  console.table([accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+// let a = 5
+// console.log("5");
